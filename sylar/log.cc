62616104d1460feea7e2df863ca1b4362c214c8b
alacrity-aya <horc5555@gmail.com>
2025-02-09 18:23:43 +0800

    formatter Part I

--- sylar/log.cc
+++ sylar/log.cc
@@ -1,8 +1,46 @@
 #include "log.h"
 #include <iostream>
+#include <string>
 #include <utility>
+#include <vector>
 
 namespace sylar {
+
+const char* LogLevel::to_string(LogLevel::Level level)
+{
+#if 0
+    
+    switch (level) {
+#define XX(name)         \
+    case LogLevel::name: \
+        return #name;    \
+        break;
+
+        XX(UNKNOWN);
+        XX(DEBUG);
+        XX(INFO);
+        XX(WARN);
+        XX(ERROR);
+        XX(FATAL);
+
+#undef XX
+
+    default:
+        return "UNKNOW";
+    }
+    return "UNKNOW";
+#endif // 0
+    static constexpr std::array<const char*, 6> level_strings = {
+        "UNKNOWN", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+    };
+
+    // 将枚举转换为下标
+    auto index = static_cast<std::size_t>(level);
+    if (index < level_strings.size()) {
+        return level_strings.at(index);
+    }
+    return "UNKNOWN";
+}
 
 // TODO: this function can be optimized by using emplace
 void Logger::add_appender(LogAppender::ptr appender) { _appenders.push_back(appender); }
@@ -23,7 +61,7 @@ Logger::Logger(std::string name)
     : _name { std::move(name) }
 {
 }
-void Logger::log(LogLevel level, LogEvent::ptr event)
+void Logger::log(LogLevel::Level level, LogEvent::ptr event)
 {
     // only outputs higher logs of higher level
     if (level >= _level) {
@@ -56,18 +94,123 @@ FileLogAppender::FileLogAppender(const std::string& filename)
 {
 }
 
-void FileLogAppender::log(LogLevel level, LogEvent::ptr event)
+void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event)
 {
     if (level >= _level) {
         _filestream << _formatter->format(event);
     }
 }
 
-void StdoutAppender::log(LogLevel level, LogEvent::ptr event)
+void StdoutAppender::log(LogLevel::Level level, LogEvent::ptr event)
 {
     if (level >= _level) {
         std::cout << _formatter->format(event);
     }
 }
+
+LogFormatter::LogFormatter(const std::string& pattern)
+    : _pattern { pattern }
+{
+}
+
+void LogFormatter::init()
+{
+    // str, format, type
+    std::vector<std::tuple<std::string, std::string, int>> vec;
+    std::string nstr;
+    for (size_t i = 0; i < _pattern.size(); ++i) {
+        if (_pattern[i] != '%') {
+            nstr.append(1, _pattern[i]);
+            continue;
+        }
+
+        if ((i + 1) < _pattern.size()) {
+            if (_pattern[i + 1] == '%') {
+                nstr.append(1, '%');
+                continue;
+            }
+        }
+
+        size_t n = i + 1;
+        int fmt_status = 0;
+        size_t fmt_begin = 0;
+
+        std::string str;
+        std::string fmt;
+        while (n < _pattern.size()) {
+            if (!fmt_status && (!isalpha(_pattern[n]) && _pattern[n] != '{' && _pattern[n] != '}')) {
+                str = _pattern.substr(i + 1, n - i - 1);
+                break;
+            }
+            if (fmt_status == 0) {
+                if (_pattern[n] == '{') {
+                    str = _pattern.substr(i + 1, n - i - 1);
+                    // std::cout << "*" << str << std::endl;
+                    fmt_status = 1; // 解析格式
+                    fmt_begin = n;
+                    ++n;
+                    continue;
+                }
+            } else if (fmt_status == 1) {
+                if (_pattern[n] == '}') {
+                    fmt = _pattern.substr(fmt_begin + 1, n - fmt_begin - 1);
+                    // std::cout << "#" << fmt << std::endl;
+                    fmt_status = 0;
+                    ++n;
+                    break;
+                }
+            }
+            ++n;
+            if (n == _pattern.size()) {
+                if (str.empty()) {
+                    str = _pattern.substr(i + 1);
+                }
+            }
+        }
+
+        if (fmt_status == 0) {
+            if (!nstr.empty()) {
+                vec.push_back(std::make_tuple(nstr, std::string(), 0));
+                nstr.clear();
+            }
+            vec.push_back(std::make_tuple(str, fmt, 1));
+            i = n - 1;
+        } else if (fmt_status == 1) {
+            std::cout << "pattern parse error: " << _pattern << " - " << _pattern.substr(i) << std::endl;
+            _error = true;
+            vec.push_back(std::make_tuple("<<pattern_error>>", fmt, 0));
+        }
+    }
+
+    if (!nstr.empty()) {
+        vec.push_back(std::make_tuple(nstr, "", 0));
+    }
+    // std::cout << m_items.size() << std::endl;
+}
+
+std::string LogFormatter::format(LogLevel::Level level, LogEvent::ptr event)
+{
+    std::stringstream ss;
+    for (auto& iter : _items) {
+        iter->format(ss, level, event);
+    }
+    return ss.str();
+}
+
+class MessageFormatItem : public LogFormatter::FormatItem {
+public:
+    void format(std::ostream& os, LogLevel::Level level, LogEvent::ptr event) override
+    {
+        os << event->get_content();
+    }
+};
+
+class LevelFormatItem : public LogFormatter::FormatItem {
+public:
+    void format(std::ostream& os, LogLevel::Level level, LogEvent::ptr event) override
+    {
+        os << LogLevel::to_string(level);
+    }
+};
 
 }
--- sylar/log.h
+++ sylar/log.h
@@ -3,17 +3,28 @@
 
 #include <cstdint>
 #include <fstream>
+#include <functional>
 #include <list>
+#include <map>
 #include <memory>
+#include <ostream>
+#include <sstream>
 #include <string>
+#include <vector>
 namespace sylar {
 
-enum class LogLevel : uint8_t {
-    DEBUG = 1,
-    INFO = 2,
-    WARN = 3,
-    ERROR = 4,
-    FATAL = 5,
+class LogLevel {
+public:
+    enum Level : uint8_t {
+        UNKNOWN = 0,
+        DEBUG = 1,
+        INFO = 2,
+        WARN = 3,
+        ERROR = 4,
+        FATAL = 5,
+    };
+
+    static const char* to_string(LogLevel::Level level);
 };
 
 // log event
@@ -21,12 +32,43 @@ class LogEvent {
 public:
     using ptr = std::shared_ptr<LogEvent>;
     LogEvent();
+    /**
+     * @brief 返回文件名
+     */
+    [[nodiscard]] const char* get_file() const { return _file; }
+
+    /**
+     * @brief 返回行号
+     */
+    [[nodiscard]] int32_t get_line() const { return _line; }
+
+    /**
+     * @brief 返回耗时
+     */
+    [[nodiscard]] uint32_t get_elapse() const { return _elapse; }
+
+    /**
+     * @brief 返回线程ID
+     */
+    [[nodiscard]] uint32_t get_thread_id() const { return _thread_id; }
+
+    /**
+     * @brief 返回协程ID
+     */
+    [[nodiscard]] uint32_t get_fiberId() const { return _fiber_id; }
+
+    /**
+     * @brief 返回时间
+     */
+    [[nodiscard]] uint64_t get_time() const { return _time; }
+
+    [[nodiscard]] std::string get_content() const { return _content; }
 
 private:
     const char* _file = nullptr; // file name
     int32_t _line = 0; // the number of line
-    uint32_t thread_id = 0; // thread id
-    uint32_t fiber_id = 0; // fiber id
+    uint32_t _thread_id = 0; // thread id
+    uint32_t _fiber_id = 0; // fiber id
     int64_t _time = 0; // time stamp
     uint32_t _elapse = 0; // millseconds since the program started
     std::string _content;
@@ -36,9 +78,21 @@ class LogFormatter {
 public:
     using ptr = std::shared_ptr<LogFormatter>;
 
-    std::string format(LogEvent::ptr event);
+    std::string format(LogLevel::Level level, LogEvent::ptr event);
+    LogFormatter(const std::string& pattern);
+
+    class FormatItem {
+    public:
+        using ptr = std::shared_ptr<FormatItem>;
+        virtual ~FormatItem();
+        virtual void format(std::ostream& os, LogLevel::Level level, LogEvent::ptr event) = 0;
+    };
 
 private:
+    void init();
+    std::string _pattern;
+    std::vector<FormatItem::ptr> _items;
+    bool _error;
 };
 
 // log output location
@@ -47,12 +101,12 @@ public:
     using ptr = std::shared_ptr<LogAppender>;
     virtual ~LogAppender() = default;
 
-    virtual void log(LogLevel level, LogEvent::ptr event) = 0;
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
     void set_formatter(LogFormatter::ptr val) { _formatter = val; }
     [[nodiscard]] LogFormatter::ptr get_fotmatter() const { return _formatter; }
 
 protected:
-    LogLevel _level;
+    LogLevel::Level _level;
     LogFormatter::ptr _formatter;
 };
 
@@ -63,7 +117,7 @@ public:
 
     explicit Logger(std::string name = "root");
 
-    void log(LogLevel level, LogEvent::ptr event);
+    void log(LogLevel::Level level, LogEvent::ptr event);
 
     void debug(LogEvent::ptr event);
     void info(LogEvent::ptr event);
@@ -73,12 +127,12 @@ public:
 
     void add_appender(LogAppender::ptr appender);
     void del_appender(LogAppender::ptr appender);
-    [[nodiscard]] LogLevel get_level() const { return _level; }
-    void set_level(LogLevel level) { _level = level; }
+    [[nodiscard]] LogLevel::Level get_level() const { return _level; }
+    void set_level(LogLevel::Level level) { _level = level; }
 
 private:
     std::string _name; // name of log
-    LogLevel _level; // log if level fit the _level here
+    LogLevel::Level _level; // log if level fit the _level here
     std::list<LogAppender::ptr> _appenders; // the lists of appenders
 };
 
@@ -86,7 +140,7 @@ private:
 class StdoutAppender : public LogAppender {
 public:
     using ptr = std::shared_ptr<StdoutAppender>;
-    void log(LogLevel level, LogEvent::ptr event) override;
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
 
 private:
 };
@@ -96,7 +150,7 @@ class FileLogAppender : public LogAppender {
 
 public:
     using ptr = std::shared_ptr<FileLogAppender>;
-    void log(LogLevel level, LogEvent::ptr event) override;
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
     explicit FileLogAppender(const std::string& filename);
 
     // if open successfully, return true
